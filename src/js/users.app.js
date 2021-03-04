ff4a6200d9cff684b686b92322b36244c02d441d
Ryan Drobel <vim>
2021-03-04 11:36:20 -0600

    Pull a list of users from the API

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -1,12 +1,31 @@
 var usersApp = (function() {
 
-    return {
-      load: function(){
-        alert('LOADED');
+    function viewUsers(){
+
+        let uri = `${window.location.origin}/api/users`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', uri);
+      
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+      
+        xhr.send();
+      
+        xhr.onload = function(){
+          let data = JSON.parse(xhr.response);
+          console.log(data);
+        }
       }
-    }
+
+      return {
+        load: function(){
+          // alert('LOADED');
+          viewUsers();
+        }
+      }
   
   })();
   
   usersApp.load();
-  
